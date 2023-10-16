b41fec4012103a8069d8d0a94cb0dc01a381d869
Sasha Anshukov <buenosdias2@yandex.ru>
2023-10-16 16:19:05 +0300

    half done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Bot from "./components/Bot/Bot";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Bot />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Bot/Bot.jsx
@@ -0,0 +1,43 @@
+import { useEffect } from "react";
+
+const Bot = () => {
+  useEffect(() => {
+    const send_message = () => {
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          chat_id: "180647202",
+          text: "Hello world",
+        }),
+      };
+
+      // Отправка запроса
+      fetch(
+        "https://api.telegram.org/bot6361219714:AAFR9GOjseY5zrknZUZgaIb28DngeLMia14/sendMessage",
+        options
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Ответ от API Telegram:", data);
+        })
+        .catch((error) => {
+          console.error("Ошибка:", error);
+        });
+    };
+
+    const targetTime = new Date("2023-10-16T16:01:00");
+    const currentTime = new Date();
+    const delay = targetTime - currentTime;
+
+    if (delay > 0) {
+      setTimeout(send_message, delay);
+    }
+  }, []);
+
+  return <div>Чат-бот для отправки сообщения</div>;
+};
+
+export default Bot;
